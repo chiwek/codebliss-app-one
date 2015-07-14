f810a3ed1abc76403a49d6bfaa663c5812bcccca
chiwek <chiwek@gmail.com>
2015-07-14 20:04:57 +0200

    added shopify_app generated install pages

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "db63115c282d105bb279bf3f5cee4fbc"
+  config.secret = "e6630cee3ba5b67d50c5cac3b19238b3"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
