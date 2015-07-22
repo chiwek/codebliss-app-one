60cbbaa7e55be156d7ac2e5f60e5b8c351c14342
chiwek <chiwek@gmail.com>
2015-07-22 18:15:50 +0200

    set product read/write permissions

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "db63115c282d105bb279bf3f5cee4fbc"
   config.secret = "e6630cee3ba5b67d50c5cac3b19238b3"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_products, write_products"
   config.embedded_app = true
   #http://codebliss-app-one.herokuapp.com/
 end
