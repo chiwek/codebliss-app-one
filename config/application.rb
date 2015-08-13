2b2c5b99c6af190faf5fc54862c2ee622eaa9e5c
chiwek <chiwek@gmail.com>
2015-08-13 18:38:40 +0200

    Uninstall webhook

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module AppOne
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.delete ActionDispatch::ParamsParser
   end
 end
+
