aa8285c02cf6c09219faf4f9ec60830a1b9ffbfb
chiwek <chiwek@gmail.com>
2015-08-13 21:35:51 +0200

    Uninstall webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,10 +1,21 @@
+require 'base64'
+require 'openssl'
+    
 class WebhooksController < ApplicationController
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
   
   def uninstall
     puts "uninstall fired"
-    shop = Shop.find_or_initialize_by(shopify_domain: ShopifyAPI::Shop.current.domain)
-    charge = ShopifyAPI::RecurringApplicationCharge.find(shop.shopify_reccuring_charge_id)
-    charge.delete    
+    puts env["HTTP_X_SHOPIFY_HMAC_SHA256"]
+    
+    digest  = OpenSSL::Digest::Digest.new('sha256')
+    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, ShopifyApp.configure.secret, data)).strip
+    verified = calculated_hmac == hmac_header
+    
+    puts verified
+    
+    #shop = Shop.find_or_initialize_by(shopify_domain: ShopifyAPI::Shop.current.domain)
+    #charge = ShopifyAPI::RecurringApplicationCharge.find(shop.shopify_reccuring_charge_id)
+    #charge.delete    
   end
 end
