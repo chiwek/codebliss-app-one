a0cb2950a84d73f5021082e9b51e3629fde19f94
chiwek <chiwek@gmail.com>
2015-08-13 17:55:30 +0200

    Uninstall webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,4 +1,6 @@
 class WebhooksController < ApplicationController
+  before_action :login_again_if_different_shop
+  around_filter :shopify_session
   def uninstall
     puts "uninstall fired"
     @charges = ShopifyAPI::RecurringApplicationCharge.all
