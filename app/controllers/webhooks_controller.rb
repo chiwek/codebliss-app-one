ba167833d789ba1984e8a3d5fab996d27cff28be
chiwek <chiwek@gmail.com>
2015-08-14 15:59:27 +0200

    Uninstall webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -6,22 +6,46 @@ class WebhooksController < ApplicationController
   
   def uninstall
     puts "uninstall fired"
+    connect_to_store
+    puts "store connected"
+    
+    shop = Shop.find_or_initialize_by(shopify_domain: ShopifyAPI::Shop.current.domain)
+    charge = ShopifyAPI::RecurringApplicationCharge.find(shop.shopify_reccuring_charge_id)
+    charge.delete
+    shop.destroy
+        
+  end
+  
+  def verifiy_webhook
     data = env["HTTP_X_SHOPIFY_HMAC_SHA256"]
-    puts data
-    puts "secret:"
-    puts ShopifyApp.configuration.secret
-    
-    
     digest  = OpenSSL::Digest::Digest.new('sha256')
     val = OpenSSL::HMAC.digest(digest, ShopifyApp.configuration.secret, data)
     calculated_hmac = Base64.encode64(val).strip
     
-    verified = calculated_hmac == data
-    
-    puts verified
-    
-    #shop = Shop.find_or_initialize_by(shopify_domain: ShopifyAPI::Shop.current.domain)
-    #charge = ShopifyAPI::RecurringApplicationCharge.find(shop.shopify_reccuring_charge_id)
-    #charge.delete    
+    calculated_hmac == data
   end
+  
+  def product_new
+      data = ActiveSupport::JSON.decode(request.body.read)
+      shopify_product_id = data["id"]
+
+      Product.new_from_shopify(@s, shopify_product_id)
+
+      head :ok
+    end
+
+    private
+
+    def connect_to_store
+
+      shop_url = request.headers['HTTP_X_SHOPIFY_SHOP_DOMAIN']
+      shop_url = ("http://" + shop_url)
+
+      @s = Shop.find_by_url(shop_url)
+
+      session = ShopifyAPI::Session.new(@s.url, @s.access_token)
+      session.valid?
+      ShopifyAPI::Base.activate_session(session)
+
+    end
 end
